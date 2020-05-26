6932850a3dacd082976f1e1f473fd72abaeefbee
Matt Sherman <mwsherman@gmail.com>
2020-05-25 20:33:05 -0400

    Benchmarks & compare

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/clipperhouse/segmenter-repro
 
 go 1.14
 
-require github.com/blevesearch/segment v0.0.0-20160915185041-762005e7a34f
+require (
+	github.com/blevesearch/segment v0.9.0
+	github.com/clipperhouse/uax29 v1.6.3
+)
--- segmenter_test.go
+++ segmenter_test.go
@@ -1,11 +1,13 @@
 package segmenter_test
 
 import (
+	"bufio"
 	"bytes"
 	"io/ioutil"
 	"testing"
 
 	"github.com/blevesearch/segment"
+	"github.com/clipperhouse/uax29/words"
 )
 
 func TestLong(t *testing.T) {
@@ -29,6 +31,29 @@ func TestLong(t *testing.T) {
 	t.Logf("saw %d segments\n", segmentcount)
 }
 
+func TestCompare(t *testing.T) {
+	file, _ := ioutil.ReadFile("wikinyc-short.txt")
+
+	r := bytes.NewReader(file)
+	sc := words.NewScanner(r)
+
+	r2 := bytes.NewReader(file)
+	segmenter := segment.NewSegmenter(r2)
+
+	for {
+		if !sc.Scan() {
+			break
+		}
+		if !segmenter.Segment() {
+			break
+		}
+
+		if sc.Text() != segmenter.Text() {
+			t.Fatalf("segmenter: %q, uax29: %q", segmenter.Text(), sc.Text())
+		}
+	}
+}
+
 func TestShort(t *testing.T) {
 	b, err := ioutil.ReadFile("wikinyc-short.txt")
 	if err != nil {
@@ -48,4 +73,59 @@ func TestShort(t *testing.T) {
 	}
 
 	t.Logf("saw %d segments\n", segmentcount)
+}
+
+func BenchmarkUAX29(b *testing.B) {
+	file, err := ioutil.ReadFile("wikinyc-short.txt")
+
+	if err != nil {
+		b.Error(err)
+	}
+
+	b.ResetTimer()
+
+	count := 0
+	for i := 0; i < b.N; i++ {
+		r := bytes.NewReader(file)
+		sc := words.NewScanner(r)
+
+		c := 0
+		for sc.Scan() {
+			c++
+		}
+		if err := sc.Err(); err != nil {
+			b.Error(err)
+		}
+
+		count = c
+	}
+	b.Logf("%d tokens\n", count)
+}
+
+func BenchmarkSegment(b *testing.B) {
+	file, err := ioutil.ReadFile("wikinyc-short.txt")
+
+	if err != nil {
+		b.Error(err)
+	}
+
+	b.ResetTimer()
+
+	count := 0
+	for i := 0; i < b.N; i++ {
+		r := bytes.NewReader(file)
+		sc := bufio.NewScanner(r)
+		sc.Split(segment.SplitWords)
+
+		c := 0
+		for sc.Scan() {
+			c++
+		}
+		if err := sc.Err(); err != nil {
+			b.Error(err)
+		}
+
+		count = c
+	}
+	b.Logf("%d tokens\n", count)
 }
